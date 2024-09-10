7515f08fc2e7991a06ab364bd0b1cd094e5bad38
Romulo Rocha <romulorj2023@gmail.com>
2024-09-10 15:57:28 -0300

    first commit

--- public/index.php
+++ public/index.php
@@ -3,19 +3,15 @@
 use Core\Session;
 use Core\ValidationException;
 
-session_start();
+const BASE_PATH = __DIR__ . '/../';
 
-const BASE_PATH = __DIR__.'/../';
+require BASE_PATH . 'vendor/autoload.php';
 
-require BASE_PATH.'Core/functions.php';
-
-spl_autoload_register(function ($class) {
-    $class = str_replace('\\', DIRECTORY_SEPARATOR, $class);
-
-    require base_path("{$class}.php");
-});
+require BASE_PATH . 'Core/functions.php';
 
 require base_path('bootstrap.php');
+
+session_start();
 
 $router = new \Core\Router();
 $routes = require base_path('routes.php');
@@ -33,5 +29,3 @@ try {
 }
 
 Session::unflash();
-
-
---
+++ public/playground.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Collection;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$numbers = new Collection([
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    10
+]);
+echo "<pre>";
+var_dump($numbers);
+echo "</pre>";
+
+echo $numbers->filter(function ($value) {
+    return $value > 7;
+});
